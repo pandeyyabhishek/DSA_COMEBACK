f8c4b359c1bb4c8420b87811681a76c087d69cab
Abhishek Pandey <abhishekpandey8520@gmail.com>
2026-05-02 11:40:44 +0530

    linkedlist

---
+++ LinkedList/creation.java
@@ -0,0 +1,20 @@
+
+//singly linkedlist..
+public static void main(String[]args){System.out.println("LinkedList !");
+
+}
+
+public class Node {
+    int val;
+    Node next;
+
+    Node(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class LL {
+    Node* head;
+    
+}
